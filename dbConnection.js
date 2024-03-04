bd12618c7e0426fb7dda8ed36e07664b64d3014b
faturrhmn <almaydaf.af@gmail.com>
2024-03-04 10:53:39 +0700

    first commit

---
+++ dbConnection.js
@@ -0,0 +1,17 @@
+
+var mysql = require('mysql');
+var conn = mysql.createConnection({
+  // host: '202.157.184.149',
+  // user: 'gurilaps',
+  // password: 'jaswita6banget',      
+  // database: 'gurilaps' // 
+  host: '202.157.184.149', 
+  user: 'msibenam',   
+  password: 'magang',   
+  database: 'msiblagi' 
+});
+conn.connect(function(err) {
+  if (err) throw err;
+  console.log('Database is connected successfully !');
+});
+module.exports = conn;
